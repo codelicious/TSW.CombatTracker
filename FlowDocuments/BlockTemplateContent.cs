6d86d54bd3b60343d23340bfe3567b4247ab285c
dharber <dharber+github@pobox.com>
2012-05-25 21:40:25 -0400

    Trying to add informational tooltips to attack summaries Attempting to use bindable FlowDocument elements

---
+++ FlowDocuments/BlockTemplateContent.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows;
+using System.Windows.Documents;
+
+namespace FlowDocuments
+{
+    public class BlockTemplateContent : Section
+    {
+        private static readonly DependencyProperty TemplateProperty = DependencyProperty.Register("Template", typeof(DataTemplate), typeof(BlockTemplateContent), new PropertyMetadata(OnTemplateChanged));
+
+        public DataTemplate Template
+        {
+            get
+            {
+                return (DataTemplate)GetValue(TemplateProperty);
+            }
+            set
+            {
+                SetValue(TemplateProperty, value);
+            }
+        }
+
+
+        public BlockTemplateContent()
+        {
+            Helpers.FixupDataContext(this);
+            Loaded += BlockTemplateContent_Loaded;
+        }
+
+
+        private void BlockTemplateContent_Loaded(object sender, RoutedEventArgs e)
+        {
+            GenerateContent(Template);
+        }
+
+
+        private void GenerateContent(DataTemplate template)
+        {
+            Blocks.Clear();
+            if (template != null)
+            {
+                FrameworkContentElement element = Helpers.LoadDataTemplate(template);
+                Blocks.Add((Block)element);
+            }
+        }
+
+        private void OnTemplateChanged(DataTemplate dataTemplate)
+        {
+            GenerateContent(dataTemplate);
+        }
+
+
+        private static void OnTemplateChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((BlockTemplateContent)d).OnTemplateChanged((DataTemplate)e.NewValue);
+        }
+    }
+}
---
+++ FlowDocuments/Fragment.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Markup;
+
+namespace FlowDocuments
+{
+    [ContentProperty("Content")]
+    public class Fragment : FrameworkElement
+    {
+        private static readonly DependencyProperty ContentProperty = DependencyProperty.Register("Content", typeof(FrameworkContentElement), typeof(Fragment));
+
+        public FrameworkContentElement Content
+        {
+            get
+            {
+                return (FrameworkContentElement)GetValue(ContentProperty);
+            }
+            set
+            {
+                SetValue(ContentProperty, value);
+            }
+        }
+    }
+}
