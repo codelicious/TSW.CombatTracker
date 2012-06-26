43933ff9cc95d5caf07d47bd0747bf0e23754807
dharber <dharber+github@pobox.com>
2012-06-26 17:04:17 -0400

    Add a diagnostic utility to dump unhandled combat log entries

---
+++ ShowLogSkips/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using TSW.CombatParser;
+
+namespace ShowLogSkips
+{
+	class Program
+	{
+		static CombatParser parser;
+
+		static void Main(string[] args)
+		{
+			parser = new CombatParser();
+			parser.Skipped += parser_SkippedLine;
+
+			foreach (string arg in args)
+			{
+				ProcessEntry(arg);
+			}
+		}
+
+		static void ProcessEntry(string entry)
+		{
+			DirectoryInfo dir = new DirectoryInfo(Environment.CurrentDirectory);
+			FileInfo[] files = dir.GetFiles(entry);
+			foreach (FileInfo file in files)
+				ProcessFile(file);
+		}
+
+		static void ProcessFile(FileInfo file)
+		{
+			StreamReader r = new StreamReader(file.FullName);
+			string line;
+
+			while ((line = r.ReadLine()) != null)
+				parser.Parse(line);
+
+			r.Close();
+		}
+
+		static void parser_SkippedLine(object sender, SkippedEventArgs e)
+		{
+			Console.WriteLine(e.Line);
+		}
+	}
+}
