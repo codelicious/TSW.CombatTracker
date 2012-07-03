c051f291cdc9a1d22f87a54ee57bd3e44850e09e
dharber <dharber+github@pobox.com>
2012-07-03 17:20:25 -0400

    Add TSW script generation

---
+++ TSW.CombatTracker/Commands.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace TSW.CombatTracker
+{
+	public class CommandActions
+	{
+		public static RoutedCommand GenerateScripts { get; private set; }
+
+		static CommandActions()
+		{
+			GenerateScripts = new RoutedCommand();
+		}
+	}
+}
